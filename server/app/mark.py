e123d220621a7c14b34ddbd791c8fe15e00e2f23
Guen Nhat King <GuenNhatKing@gmail.com>
2025-11-16 21:30:35 +0700

    refactor: remove unused check_mark function and related imports from mark.py

--- server/app/mark.py
+++
@@ -1,6 +0,0 @@
-from django.db import transaction
-from models import *
-
-# def check_mark(exam_id: str):
-#     exam_records = Exam.objects.filter(exam_id=exam_id)
-#     examinee_papers = ExamineePaper.objects.filter()
