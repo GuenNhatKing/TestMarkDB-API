706b345caf497a12d6b3ea4bbb016aee5966d5dd
Guen Nhat King <GuenNhatKing@gmail.com>
2025-11-19 20:39:29 +0700

    fix: remove background execution for HTTP server in entrypoint script

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -8,8 +8,8 @@ if [ "$PROD" = "true" ]; then
   if [ "$RUN_APP" = "true" ]; then
     exec uwsgi --ini ./uwsgi.ini
   elif [ "$RUN_CELERY" = "true" ]; then
-    python -m http.server 8080 &
-    exec celery -A root worker --loglevel=INFO --pool=threads
+    python -m http.server 8080
+    exec celery -A root worker --loglevel=INFO
   else
     exit 1
   fi
