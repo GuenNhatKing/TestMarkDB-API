eefda6f1ff0172c283220d62705bbeab1fa45b8c
Guen Nhat King <GuenNhatKing@gmail.com>
2025-11-27 22:56:37 +0700

    fix: remove thread pool option from Celery command and enhance entrypoint script logging

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,13 +5,18 @@ cd /app/server
 python manage.py collectstatic --noinput
 
 if [ "$PROD" = "true" ]; then
+  echo "Running in production mode"
   if [ "$RUN_APP" = "true" ]; then
+    echo "Starting uWSGI application server"
     exec uwsgi --ini ./uwsgi.ini
   elif [ "$RUN_CELERY" = "true" ]; then
+    echo "Starting Celery worker"
     exec celery -A root worker --loglevel=INFO
   else
+    echo "No valid RUN_APP or RUN_CELERY flag set. Exiting."
     exit 1
   fi
 else
+  echo "Running in development mode"
   exec "$@"
 fi
