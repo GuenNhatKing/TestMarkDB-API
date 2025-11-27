d869f01d4deecc55c9bcf60f4545855d672e2bee
Guen Nhat King <GuenNhatKing@gmail.com>
2025-11-27 21:56:36 +0700

    fix: remove HTTP server execution from entrypoint script

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -8,7 +8,6 @@ if [ "$PROD" = "true" ]; then
   if [ "$RUN_APP" = "true" ]; then
     exec uwsgi --ini ./uwsgi.ini
   elif [ "$RUN_CELERY" = "true" ]; then
-    python -m http.server 8080
     exec celery -A root worker --loglevel=INFO
   else
     exit 1
