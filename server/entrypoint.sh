4ba33ff3027370958707cfbb092df266d650c900
Guen Nhat King <GuenNhatKing@gmail.com>
2025-11-28 08:54:13 +0700

    fix: update entrypoint script and docker-compose commands for improved execution flow

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 set -e
 cd /app/server
-echo "Starting entrypoint script"
 
 if [ "$PROD" = "true" ]; then
-  echo "Running in production mode"
   if [ "$RUN_APP" = "true" ]; then
     echo "Starting uWSGI application server"
     python manage.py collectstatic --noinput
     exec uwsgi --ini ./uwsgi.ini
   elif [ "$RUN_CELERY" = "true" ]; then
     echo "Starting Celery worker"
-    exec celery -A root worker --loglevel=INFO --pool=threads
+    exec celery -A root worker --loglevel=INFO
   else
     echo "No valid RUN_APP or RUN_CELERY flag set. Exiting."
     exit 1
   fi
 else
   echo "Running in development mode"
-  python manage.py collectstatic --noinput
-  exec "$@"
+  if [ "$@" = "RUN_APP" ]; then
+    echo "Starting uWSGI application server"
+    python manage.py collectstatic --noinput
+    exec uwsgi --ini ./uwsgi.ini
+  elif [ "$@" = "RUN_CELERY" ]; then
+    echo "Starting Celery worker"
+    exec celery -A root worker --loglevel=INFO
+  else
+    echo "No valid RUN_APP or RUN_CELERY flag set. Exiting."
+    exit 1
+  fi
 fi
